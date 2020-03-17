be6b24689ffd65674725c1b6242562d631657250
valdiney <valdiney.2@hotmail.com>
2020-03-16 23:47:41 -0300

    Icones nativos

--- public/js/firebaseConfig.js
+++ public/js/firebaseConfig.js
@@ -8,3 +8,16 @@ var config = {
 };
 
 firebase.initializeApp(config);
+
+
+
+var fileButton = document.querySelector('#img');
+//var img = document.querySelector("#enviar_imagem");
+
+fileButton.addEventListener('change', function(e) {
+	var file = e.target.files[0];
+	var storageRef = firebase.storage().ref('fotos/'+file.name);
+
+	var task = storageRef.put(file);
+
+})
